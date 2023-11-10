09b87a3c439a347dc84545845cd4e7c5376b7659
lorenzo-neri <lorenzo21neri@gmail.com>
2023-11-10 13:11:13 +0100

    work in admin/projects show

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -8,9 +8,11 @@
 
         <hr>
 
-        {{$projects->links('pagination::bootstrap-5')}}
+        {{ $projects->links('pagination::bootstrap-5') }}
 
-        <a style="width: 75px; aspect-ratio:1/1" class="btn btn-success d-flex align-items-center justify-content-center rounded-circle position-fixed bottom-0 end-0 m-4" href="{{ route('admin.projects.create') }}"><i class="fa-solid fa-plus fa-xl"></i></a>
+        <a style="width: 75px; aspect-ratio:1/1"
+            class="btn btn-success d-flex align-items-center justify-content-center rounded-circle position-fixed bottom-0 end-0 m-4"
+            href="{{ route('admin.projects.create') }}"><i class="fa-solid fa-plus fa-xl"></i></a>
 
         @if (session('status'))
             <div class="alert alert-success" role="alert">
@@ -51,45 +53,54 @@
                             <td class="align-middle">link web</td>
 
                             <td class="align-middle">{{ $project->tech }}</td>
-                          
+
                             {{-- SHOW --}}
                             <td class="align-middle text-center">
-                                <a class="btn btn-primary" href="{{ route('admin.projects.show', $project->slug) }}"><i class="fa-solid fa-magnifying-glass"></i></a>
+                                <a class="btn btn-primary" href="{{ route('admin.projects.show', $project->slug) }}"><i
+                                        class="fa-solid fa-magnifying-glass"></i></a>
 
-                            {{-- EDIT --}}
-                                <a class="btn btn-warning" href="{{ route('admin.projects.edit', $project->slug) }}"><i class="fa-solid fa-pen-to-square"></i></a>
+                                {{-- EDIT --}}
+                                <a class="btn btn-warning" href="{{ route('admin.projects.edit', $project->slug) }}"><i
+                                        class="fa-solid fa-pen-to-square"></i></a>
 
 
-                            {{-- DELETE --}}
+                                {{-- DELETE --}}
                                 {{-- <a class="btn btn-danger" href="{{ route('admin.projects.destroy', $project) }}">Delete</a> --}}
 
-                                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalId-{{$project->id}}">
+                                <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                    data-bs-target="#modalId-{{ $project->id }}">
                                     <i class="fa-solid fa-trash"></i>
                                 </button>
-                                
+
                                 {{-- MODALE PER ELIMINARE ELEMENTO --}}
-                                <div class="modal fade" id="modalId-{{$project->id}}" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="modalId-{{$project->id}}" aria-hidden="true">
-                                  <div class="modal-dialog">
-                                    <div class="modal-content">
-                                      <div class="modal-header bg-danger text-white justify-content-center">
-                                        <h5 class="modal-title text-uppercase" id="modalTitleId-{{$project->id}}">Attenzione!</h5>
-                                      </div>
-                                      <div class="modal-body fs-5">
-                                        Il progetto <strong>{{$project->id}}</strong> - <strong>{{$project->title}}</strong> sta per essere eliminato!
-                                      </div>
-                                      <div class="modal-footer justify-content-between">
-                                        <button type="button" class="btn btn-primary" data-bs-toggle="modal">
-                                            <i class="fa-solid fa-angle-left"></i> Back
-                                        </button>
-                                        {{-- non confondere destroy con delete --}}
-                                        <form action="{{route ('admin.projects.destroy', $project->slug)}}" method="POST">
-                                            @csrf
-                                            @method('delete')
-                                            <button type="submit" class="btn btn-danger">Delete <i class="fa-solid fa-trash"></i></button>
-                                        </form>
-                                      </div>
+                                <div class="modal fade" id="modalId-{{ $project->id }}" data-backdrop="static"
+                                    data-keyboard="false" tabindex="-1" aria-labelledby="modalId-{{ $project->id }}"
+                                    aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header bg-danger text-white justify-content-center">
+                                                <h5 class="modal-title text-uppercase"
+                                                    id="modalTitleId-{{ $project->id }}">Attenzione!</h5>
+                                            </div>
+                                            <div class="modal-body fs-5">
+                                                Il progetto <strong>{{ $project->id }}</strong> -
+                                                <strong>{{ $project->title }}</strong> sta per essere eliminato!
+                                            </div>
+                                            <div class="modal-footer justify-content-between">
+                                                <button type="button" class="btn btn-primary" data-bs-toggle="modal">
+                                                    <i class="fa-solid fa-angle-left"></i> Back
+                                                </button>
+                                                {{-- non confondere destroy con delete --}}
+                                                <form action="{{ route('admin.projects.destroy', $project->slug) }}"
+                                                    method="POST">
+                                                    @csrf
+                                                    @method('delete')
+                                                    <button type="submit" class="btn btn-danger">Delete <i
+                                                            class="fa-solid fa-trash"></i></button>
+                                                </form>
+                                            </div>
+                                        </div>
                                     </div>
-                                  </div>
                                 </div>
                             </td>
                         </tr>
